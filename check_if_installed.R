fc321548a54c12df45411904921d41d41de9c43b
Brian Yang <anan0329@yahoo.com.tw>
2021-06-04 00:49:46 +0800

    Update check_if_installed.R

--- check_if_installed.R
+++ check_if_installed.R
@@ -5,7 +5,7 @@ package.check <- lapply(
   FUN = function(x) {
     if (!require(x, character.only = TRUE)) {
       install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
+      # library(x, character.only = TRUE)
     }
   }
 )
